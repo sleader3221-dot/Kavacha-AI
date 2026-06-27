343fb366df32fa252cf38cdae376e74ae3e2644d
Hi <H@gmail.com>
2026-06-27 12:37:12 +0530

    Prerender homepage for Zoho Slate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { KavachaApp } from "@/components/kavacha-app";
 import { getDashboardSnapshot } from "@/lib/synthetic-data";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 export default function Home() {
   const snapshot = getDashboardSnapshot();
   return <KavachaApp initialSnapshot={snapshot} />;
